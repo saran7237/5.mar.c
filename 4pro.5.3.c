93e068a0c26b023a7ce502529ab75c6443e16a04
saran7237 <36067371+saran7237@users.noreply.github.com>
2018-03-05 15:59:03 +0530

    Create 4pro.5.3.c

---
+++ 4pro.5.3.c
@@ -0,0 +1,15 @@
+def checkrange(n):
+	if n in range(1,11):
+		print('yes')
+	else :
+		print('no')
+def main():
+	try:
+		n=int(input())
+		checkrange(n)
+	except:
+		print('invalid')
+		
+main()
+return 0;
+}
